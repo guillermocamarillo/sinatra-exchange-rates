266a4b5b513f422ea9ecba70493591a00cc499e1
guillermocamarillo <137141464+guillermocamarillo@users.noreply.github.com>
2023-07-19 04:25:29 +0000

    setting up exhange rates project

--- app.rb
+++ app.rb
@@ -1,9 +1,30 @@
 require "sinatra"
 require "sinatra/reloader"
+require "open-uri"
+require "json"
+require "http"
+
+
+currency_exchange_url = "https://api.exchangerate.host/convert?from=USD&to=INR"
+
+all_symbols_url = "https://api.exchangerate.host/symbols"
+
+symbols_data = URI.open(all_symbols_url).read
+
+parse_symbols_data = JSON.parse(symbols_data)
+
+filtered_symbolds_data = parse_symbols_data.fetch("symbols")
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @keys = filtered_symbolds_data.keys
+
+  erb(:homepage)
+end
+
+get("/:symbol") do
+  @symbol = params.fetch("symbol")
+  @keys = filtered_symbolds_data.keys
+  
+
+  erb(:symbol)
 end
